53e65d5d5694d8551b75431d245cd4c40796b2da
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-04-08 14:10:45 +0100

    Fix styling

--- src/routes/Header.jsx
+++ src/routes/Header.jsx
@@ -4,7 +4,7 @@ import Headerlink from '../components/headerlinks';
 const Header = () => {
   const links = [{ link: 'BOOKS', path: '/' }, { link: 'CATEGORY', path: 'category' }];
   return (
-    <div className='overall'>
+    <div className="overall">
       <Headerlink links={links} />
       <Outlet />
     </div>
