3b40be4519a953fa5500746f1bedb83b8f77a6dc
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-04-08 14:07:58 +0100

    Fix styling

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -23,65 +23,68 @@ const Books = () => {
 
   return (
     <ul>
-      {books.map((element) => (
-        <section key={element.item_id} className="booksStorage" id={element.item_id}>
-          <ul>
-            <li className="cat">{element.category}</li>
-            <li className="tit"><h3>{element.title}</h3></li>
-            <li className="blued">{element.author}</li>
-            <li>
-              <button type="button" className="links buttons blued">Comments</button>
-              {' '}
-              <span className="shortline">|</span>
-              {' '}
-              <button type="button" className="links buttons blued" onClick={() => handleDelete(`${element.item_id}`)}>Remove</button>
-              {' '}
-              <span className="shortline">|</span>
-              {' '}
-              <button type="button" className="links buttons blued">Edit</button>
-            </li>
-          </ul>
-          <div>
-            <div className="status" key={element.key} />
-            <ul className="progresssect">
-              <li className="percentcontainer">
-                <div
-                  className="percentbar"
-                  style={{
-                    width: '5rem',
-                    height: '5rem',
-                    borderRadius: '50%',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    background: `conic-gradient(rgba(106, 177, 243, 0.945) ${element.degree}deg, #ededed 0deg)`,
-                  }}
-                />
-              </li>
+      {books.map((element) => {
+        const progressStyle = {
+          width: '5rem',
+          height: '5rem',
+          borderRadius: '50%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          background: `conic-gradient(rgba(106, 177, 243, 0.945) ${element.degree}deg, #ededed 0deg)`,
+        };
+        return (
+          <section key={element.item_id} className="booksStorage" id={element.item_id}>
+            <ul>
+              <li className="cat">{element.category}</li>
+              <li className="tit"><h3>{element.title}</h3></li>
+              <li className="blued">{element.author}</li>
               <li>
-                <article>
-                  <h3 className="percent">
-                    {element.percentage}
-                    %
-                  </h3>
-                </article>
-                <article className="cat small">COMPLETED</article>
+                <button type="button" className="links buttons blued">Comments</button>
+                {' '}
+                <span className="shortline">|</span>
+                {' '}
+                <button type="button" className="links buttons blued" onClick={() => handleDelete(`${element.item_id}`)}>Remove</button>
+                {' '}
+                <span className="shortline">|</span>
+                {' '}
+                <button type="button" className="links buttons blued">Edit</button>
               </li>
-
             </ul>
-          </div>
-          {' '}
-          <span className="vert" />
-          <ul>
-            <li className="cat"><h5 className="curr">CURRENT CHAPTER</h5></li>
-            <li>
-              Chapter
-              {element.chapter}
-            </li>
-            <li><button type="button" className="btn">UPDATE PROGRESS</button></li>
-          </ul>
-        </section>
-      ))}
+            <div>
+              <div className="status" key={element.key} />
+              <ul className="progresssect">
+                <li className="percentcontainer">
+                  <div
+                    className="percentbar"
+                    style={progressStyle}
+                  />
+                </li>
+                <li>
+                  <article>
+                    <h3 className="percent">
+                      {element.percentage}
+                      %
+                    </h3>
+                  </article>
+                  <article className="cat small">COMPLETED</article>
+                </li>
+
+              </ul>
+            </div>
+            {' '}
+            <span className="vert" />
+            <ul>
+              <li className="cat"><h5 className="curr">CURRENT CHAPTER</h5></li>
+              <li>
+                Chapter
+                {element.chapter}
+              </li>
+              <li><button type="button" className="btn">UPDATE PROGRESS</button></li>
+            </ul>
+          </section>
+        );
+      })}
     </ul>
   );
 };
